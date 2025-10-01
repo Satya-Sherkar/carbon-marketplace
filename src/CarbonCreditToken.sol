6acb5d758adfb951896305c6a96e518e0e8721d8
Satyam Sherkar <satyasherkar@gmail.com>
2025-10-01 07:31:26 +0000

    Natspec Documentation added

--- src/CarbonCreditToken.sol
+++ src/CarbonCreditToken.sol
@@ -3,21 +3,34 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ *  @title CarbonCreditToken Test
+ *  @author Satyam Sherkar
+ *  @notice This contract implements an ERC20 token for carbon credits
+ *  @dev Also see OpenZeppelin's ERC20 for inherited properties.
+ */
 contract CarbonCreditToken is ERC20 {
-    // Errors
     error InvalidAddress();
     error InsufficientBalance();
     error UnauthorizedAccount();
 
-    // Immutable
+    /**
+     *  @notice Address of the contract owner(marketplace contract)
+     */
     address public immutable OWNER;
 
-    // Constructor
-    constructor(address _owner) ERC20("Carbon Credit Token", "CCT") {
-        OWNER = _owner;
+    /**
+     * @notice Initializes the contract with an owner address
+     * @dev Sets up the ERC20 token with name "Carbon Credit Token" and symbol "CCT"
+     * @param owner Address (Marketplace Contract) that will have permission to mint and burn tokens and all other functions of this contract.
+     */
+    constructor(address owner) ERC20("Carbon Credit Token", "CCT") {
+        OWNER = owner;
     }
 
-    // Modifier
+    /**
+     * @notice Restricts function access to contract owner
+     */
     modifier onlyOwner() {
         if (msg.sender != OWNER) {
             revert UnauthorizedAccount();
@@ -25,7 +38,9 @@ contract CarbonCreditToken is ERC20 {
         _;
     }
 
-    // Functions
+    /**
+     * @notice Mints new tokens to a specified address
+     */
     function mint(address to, uint256 amount) external onlyOwner {
         if (to == address(0)) {
             revert InvalidAddress();
@@ -33,14 +48,26 @@ contract CarbonCreditToken is ERC20 {
         _mint(to, amount);
     }
 
+    /**
+     * @notice Burns tokens from a holder's address, use for retiring credits
+     * @param holder Address from which to burn tokens(Retire)
+     * @param amount Number of tokens to burn(Retire)
+     */
     function burn(address holder, uint256 amount) external onlyOwner {
-        if (amount < balanceOf(holder)) {
+        if (amount > balanceOf(holder)) {
             revert InsufficientBalance();
         }
         _burn(holder, amount);
     }
 
-    function approve_(address holder, address spender, uint256 value) external onlyOwner {
+    /**
+     * @notice Approves spending of tokens on behalf of a holder, use for sending tokens for listing in marketplace contract.
+     */
+    function approve_(
+        address holder,
+        address spender,
+        uint256 value
+    ) external onlyOwner {
         _approve(holder, spender, value);
     }
 }
